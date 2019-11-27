769d23e70fe7dbf2add78a2becbd3b4c6f09da81
asmaa <asmathabet2009@hotmail.com>
2019-11-27 21:47:39 +0200

    add queries to fetch,call, display books details

---
+++ client/src/queries/index.js
@@ -0,0 +1,51 @@
+import {gql} from 'apollo-boost';
+
+// (!) after each type means not null - value from type can't be empty
+const getBooksQuery = gql`
+{
+    books{
+        name
+        id  
+    }
+}
+`
+
+
+const getAuthorsQuery = gql`
+{
+    authors{
+        name
+        id  
+    }
+}
+`
+
+const addBookMutation = gql`
+    mutation($name: String!, $genre: String!, $authorId: ID!){
+        addBook(name:$name, genre:$genre ,authorId:$authorId){
+            name
+            id
+        }
+    }
+`
+
+const getBookQuery = gql`
+    query($id: ID!){
+        book(id: $id){
+            id
+            name
+            genre
+            author{
+                id 
+                name
+                age
+                books{
+                    name
+                    id
+                }
+            }
+        }
+    }
+`
+
+export {getBooksQuery, getAuthorsQuery, addBookMutation, getBookQuery};
--- server/app.js
+++ server/app.js
@@ -2,11 +2,21 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const app = express();
 
+// allow cross origin requests
+app.use(cors())
 //connect to mlab - mongoDB
-mongoose.connect('mongodb+srv://asmaa:123@cluster0-8kezu.mongodb.net/test?retryWrites=true&w=majority');
+mongoose
+.connect('mongodb+srv://asmaa:123@cluster0-8kezu.mongodb.net/test?retryWrites=true&w=majority',  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    })
+.then(()=>console.log('DB Connected'))  
+.catch(err => {
+    console.log(`DB Connection error: ${err.message}`);
+});
 mongoose.connection.once('open', ()=> {
     console.log('*******connected to database*******');
 })
